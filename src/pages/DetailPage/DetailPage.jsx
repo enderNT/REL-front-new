55b69f9059a0b414f88539c18c342c8133e5f038
gabrielyael.gonzalez.contractor <gabrielyael.gonzalez.contractor@bbva.com>
2023-12-06 15:05:19 -0600

    base de la maqueta

--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -1,28 +1,41 @@
-import { Image } from "antd";
+import { Collapse, Image } from "antd";
 import Layout, { Content, Footer, Header } from "antd/es/layout/layout";
+import { headerStyle, imageStyle, contentStyle, content_TitleStyle, footerStyle, content_CollapseStyle } from "./DetailPage-styles";
+import Title from "antd/es/typography/Title";
+import { COLLAPSE_ITEMS } from "./utils/constants";
 
 const DetailPage = () => {
-    const contentStyle = {
-        textAlign: 'center'
-    }
-    return (
-        <>
-            <Layout>
-                <Header></Header>
-                <Content style={contentStyle}>
-                    <Image
-                        width={200}
-                        height={200}
-                        src="https://loremipsum.io/assets/images/lorem-ipsum-generator-cicero-engraving.png"
-                        fallback={DETAIL_PAGE_IMAGE_FALLBACK}
-                    >
-
-                    </Image>
-                </Content>
-                <Footer></Footer>
-            </Layout>
-        </>
-    )
+  return (
+    <>
+      <Layout>
+        <Header
+          style={headerStyle}
+        >
+          <Image
+            alt="error to load"
+            width='100%'
+            height='100%'
+            src="https://tudepa.com/assets/devs/central-32/fachada-3.jpg"
+            style={imageStyle}
+          >
+          </Image>
+        </Header>
+        <Content
+          style={contentStyle}
+        >
+          <Title style={content_TitleStyle} level={4}>Nombre de la propiedad</Title>
+          <Title style={content_TitleStyle} level={4}>$2000</Title>
+          <Collapse 
+            ghost items={COLLAPSE_ITEMS}
+            style={content_CollapseStyle}>
+          </Collapse>
+        </Content>
+        <Footer
+          style={footerStyle}
+        ></Footer>
+      </Layout>
+    </>
+  )
 }
 
 export default DetailPage;
